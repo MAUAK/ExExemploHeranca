e2bda21141837522d095e4707766c42d1ab73e4c
MAUAK <kauamsilva360@gmail.com>
2021-10-20 21:21:18 -0300

    Classe abstrata

--- ExExemploHerança/Program.cs
+++ ExExemploHerança/Program.cs
@@ -12,20 +12,24 @@ namespace ExExemploHerança
             list.Add(new ContaPoupanca(1001, "Terry", 500.0, 0.01));
             list.Add(new ContaJuridica(1002, "Andy", 500.0, 400.0));
             list.Add(new ContaPoupanca(1003, "Joe", 500.0, 0.01));
-            list.Add(new ContaJuridica(1001, "Mae", 50.0, 400.0));
+            list.Add(new ContaJuridica(1004, "Mae", 500.0, 400.0));
 
             double soma = 0.0;
             foreach(Conta acc in list)
             {
                 soma += acc.Saldo;
             }
-            Console.WriteLine("Total do saldo de contas; " + soma.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Total do saldo de contas: " + soma.ToString("F2", CultureInfo.InvariantCulture));
 
             foreach(Conta acc in list)
             {
                 acc.Saque(10.0);
             }
-            
+            foreach (Conta acc in list)
+            {
+                Console.WriteLine("Atualização do saldo das contas "
+                + acc.Numero + ": " + acc.Saldo.ToString("F2", CultureInfo.InvariantCulture));
+            }
         }
     }
 }
